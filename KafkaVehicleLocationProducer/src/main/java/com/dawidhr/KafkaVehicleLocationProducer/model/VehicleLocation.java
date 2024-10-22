40dce1641ee85c48a8a39863e0c22673374d3cb2
DawidHr <dawiid21@gmail.com>
2024-10-22 19:34:57 +0200

    Add model information

--- KafkaVehicleLocationProducer/src/main/java/com/dawidhr/KafkaVehicleLocationProducer/model/VehicleLocation.java
+++ KafkaVehicleLocationProducer/src/main/java/com/dawidhr/KafkaVehicleLocationProducer/model/VehicleLocation.java
@@ -1,2 +1,17 @@
-package com.dawidhr.KafkaVehicleLocationProducer.model;public class VehicleLocation {
+package com.dawidhr.KafkaVehicleLocationProducer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class VehicleLocation implements Serializable {
+    private Integer userId;
+    private Integer vehicleId;
+    private Double x;
+    private Double y;
 }
