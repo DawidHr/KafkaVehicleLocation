29232697973a65b9e79164e4bb89f3f6409e12fa
DawidHr <dawiid21@gmail.com>
2024-10-22 18:25:42 +0200

    Project init

---
+++ KafkaVehicleLocationProducer/src/main/java/com/dawidhr/KafkaVehicleLocationProducer/KafkaVehicleLocationProducerApplication.java
@@ -0,0 +1,13 @@
+package com.dawidhr.KafkaVehicleLocationProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaVehicleLocationProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaVehicleLocationProducerApplication.class, args);
+	}
+
+}
---
+++ KafkaVehicleLocationProducer/src/test/java/com/dawidhr/KafkaVehicleLocationProducer/KafkaVehicleLocationProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawidhr.KafkaVehicleLocationProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaVehicleLocationProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
