c68cfac0d82d3f77a60fee02549dbfeeedd138c3
DawidHr <dawiid21@gmail.com>
2024-10-22 19:47:24 +0200

    Add service

--- KafkaVehicleLocationProducer/src/main/java/com/dawidhr/KafkaVehicleLocationProducer/service/KafkaProducer.java
+++ KafkaVehicleLocationProducer/src/main/java/com/dawidhr/KafkaVehicleLocationProducer/service/KafkaProducer.java
@@ -1,2 +1,19 @@
-package com.dawidhr.KafkaVehicleLocationProducer.service;public class KafkaProducer {
+package com.dawidhr.KafkaVehicleLocationProducer.service;
+
+import com.dawidhr.KafkaVehicleLocationProducer.model.KafkaTopic;
+import com.dawidhr.KafkaVehicleLocationProducer.model.VehicleLocation;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class KafkaProducer {
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(VehicleLocation vehicleLocation) {
+        kafkaTemplate.send(KafkaTopic.VEHICLE_LOCATION.getTopicName(), vehicleLocation.toString());
+    }
 }
