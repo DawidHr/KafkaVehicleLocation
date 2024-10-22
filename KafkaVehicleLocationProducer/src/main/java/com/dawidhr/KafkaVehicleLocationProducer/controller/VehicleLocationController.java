00b5e75a63b739a634983c3320444da372b8cdf2
DawidHr <dawiid21@gmail.com>
2024-10-22 19:37:36 +0200

    Add controller

--- KafkaVehicleLocationProducer/src/main/java/com/dawidhr/KafkaVehicleLocationProducer/controller/VehicleLocationController.java
+++ KafkaVehicleLocationProducer/src/main/java/com/dawidhr/KafkaVehicleLocationProducer/controller/VehicleLocationController.java
@@ -1,2 +1,20 @@
-package com.dawidhr.KafkaVehicleLocationProducer.controller;public class VehicleLocationController {
+package com.dawidhr.KafkaVehicleLocationProducer.controller;
+
+import com.dawidhr.KafkaVehicleLocationProducer.model.VehicleLocation;
+import com.dawidhr.KafkaVehicleLocationProducer.service.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VehicleLocationController {
+    @Autowired
+    KafkaProducer kafkaProducer;
+
+    @PostMapping("/vehicle_location")
+    public void vehicleLocation(@RequestBody VehicleLocation vehicleLocation) {
+        System.out.println(vehicleLocation.toString());
+        kafkaProducer.sendMessage(vehicleLocation);
+    }
 }
