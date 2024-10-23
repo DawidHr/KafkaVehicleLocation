6755ba27e55a36e6eb0c5e225ad287a80f52d845
DawidHr <dawiid21@gmail.com>
2024-10-23 15:37:18 +0200

    Add kafka configuration to Kafka Consumer Project

--- KafkaVehicleLocationConsumer/src/main/java/com/dawidhr/KafkaVehicleLocationConsumer/config/KafkaConfiguration.java
+++ KafkaVehicleLocationConsumer/src/main/java/com/dawidhr/KafkaVehicleLocationConsumer/config/KafkaConfiguration.java
@@ -1,16 +1,18 @@
-package com.dawidhr.KafkaVehicleLocationProducer.config;
+package com.dawidhr.KafkaVehicleLocationConsumer.config;
 
-import com.dawidhr.KafkaVehicleLocationProducer.model.KafkaTopic;
+
+import com.dawidhr.KafkaVehicleLocationConsumer.model.KafkaTopic;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,24 +22,29 @@ import java.util.Map;
 public class KafkaConfiguration {
 
     @Bean
-    public Map<String, Object> configuration() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return config;
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
     }
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(configuration());
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(3);
+        factory.getContainerProperties().setPollTimeout(3000);
+        return factory;
     }
-
     @Bean
     public NewTopic newTopic() {
         return TopicBuilder.name(KafkaTopic.VEHICLE_LOCATION.getTopicName())
