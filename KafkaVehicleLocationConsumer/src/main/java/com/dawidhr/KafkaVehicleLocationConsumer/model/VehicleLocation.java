54fb7cfbeabdfb63f5d23e5e7bd29c7aba75a5b3
Cool <dawiid21@gmail.com>
2024-11-01 17:11:11 +0100

    add annotation for VehicleLocation

--- KafkaVehicleLocationConsumer/src/main/java/com/dawidhr/KafkaVehicleLocationConsumer/model/VehicleLocation.java
+++ KafkaVehicleLocationConsumer/src/main/java/com/dawidhr/KafkaVehicleLocationConsumer/model/VehicleLocation.java
@@ -1,5 +1,6 @@
 package com.dawidhr.KafkaVehicleLocationConsumer.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,18 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
+@Entity(name = "vehicle_location")
 public class VehicleLocation implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vehicle_location_id")
+    private Long vehicleLocationId;
+    @Column(name = "user_id", nullable=false)
     private Integer userId;
+    @Column(name = "vehicle_id")
     private Integer vehicleId;
+    @Column(name = "x")
     private Double x;
+    @Column(name = "y")
     private Double y;
 }
