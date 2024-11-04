27b19f16602c2eb8366943fa8da669f525681e15
DawidHr <dawiid21@gmail.com>
2024-11-04 10:44:37 +0100

    Add sql script

--- env/sql/baza.sql
+++ env/sql/baza.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE vehicle;
 USE vehicle;
+
+CREATE TABLE `vehicle_location` (
+  `user_id` int NOT NULL,
+  `vehicle_id` int DEFAULT NULL,
+  `x` double DEFAULT NULL,
+  `y` double DEFAULT NULL,
+  `vehicle_location_id` bigint NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`vehicle_location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
